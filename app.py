57da4dad51d7d8867bb2d1a27480680fb4d54b5d
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2023-03-10 21:57:28 -0800

    Add new pages

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import jwt
 # Setup/Config
 cred = credentials.Certificate('project.json')
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://logle-e6f2a-default-rtdb.firebaseio.com/'
+    'databaseURL': 'https://asianbot-96876-default-rtdb.firebaseio.com'
 })
 root = db.reference('/')
 db_password = root.child("passwords")
@@ -27,12 +27,68 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
+def decode(token):
+    try:
+        decoded = jwt.decode(token, os.getenv("JWTSecret"), "HS256")
+    except:
+        return None
+    return decoded
+
+
 @app.route('/')
 def page_home():
-    try:
-        decoded = jwt.decode(request.cookies["token"], os.getenv("JWTSECRET"), "HS256")
-    except:
-        return render_template("homepage.html", title="Homepage", uname="")
+    token = decode(request.cookies["token"])
+    if not token:
+        return render_template("homepage.html", title="Homepage", data={})
     else:
-        return render_template("homepage.html", title="Homepage", uname=decoded['uname'])
+        return render_template("homepage.html", title="Homepage", data=token)
 
+@app.route('/student')
+def page_student():
+    if "token" not in request.cookies:
+        return render_template("student.html", title="Choose Class", data={})
+    token = decode(request.cookies["token"])
+    if not token:
+        return redirect("/?error=login")
+
+    classes = root.child("users").child(token['uname']).child("classes").get()
+    res = {}
+
+    for item in classes:
+        res[item] = root.child("classes").child(item).get()
+        res[item]["teacher_realname"] = root.child("users").child(res[item]['teacher']).child("name").get()
+
+    return render_template("student.html", title="Choose Class", data=token, classes=res)
+
+@app.route('/api/login/asdev', methods=["POST"])
+def api_login_asdev():
+    token = jwt.encode({
+        "id": str(root.child("users").child("achen10").child("id").get()),
+        "uname": "achen10",
+        "name": "Andrew Chen",
+        "iat": math.floor(datetime.datetime.timestamp(datetime.datetime.now())), # Now
+        "exp": math.floor(datetime.datetime.timestamp(datetime.datetime.now() + datetime.timedelta(days=30)))
+    }, key=os.getenv("JWTSecret"), algorithm="HS256")
+
+    res = make_response(token, 200)
+    res.set_cookie("token", token)
+
+    return res
+
+
+@app.route('/class/<path:id>', defaults={"page": "info"})
+@app.route('/class/<path:id>/<path:page>')
+def class_general(id, page):
+    if "token" not in request.cookies:
+        return render_template("student.html", title="Homepage", data={})
+    token = decode(request.cookies["token"])
+    if not token:
+        return redirect("/?error=login")
+
+    data = root.child("classes").child(str(id)).get()
+    data["teacher_realname"] = root.child("users").child(data['teacher']).child("name").get()
+
+    if not page or page not in ["work", "grades", "infractions"]:
+        page = "info"
+
+    return render_template("class.html", data=token, title=data['name'], page=page, classdata=data, classid=id)
